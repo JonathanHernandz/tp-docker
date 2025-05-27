d49639331861538e581f60b5858cf1c5ecff6b3b
Jonathan <electroc_jonathan@hotmail.com>
2025-05-27 13:28:03 -0600

    se agrego getters y setters en UsuarioDto y Entity a Tarjeta

--- tarjeta-puntos-spring/src/main/java/gd/tp/usuario/dto/UsuarioDto.java
+++ tarjeta-puntos-spring/src/main/java/gd/tp/usuario/dto/UsuarioDto.java
@@ -4,6 +4,70 @@ import gd.tp.usuario.Usuario;
 
 public class UsuarioDto {
 
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
     private Long idUsuario;
     private String nombre;
     private String correo;
